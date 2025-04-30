3e92d3b85ef93e30dc9bbff7a456eb040da857a9
Art <array.clean@gmail.com>
2025-05-01 02:46:58 +0300

    GATEWAY: organize arwave

--- gateway/src/routes/portfolio.rs
+++ gateway/src/routes/portfolio.rs
@@ -195,6 +195,11 @@ pub async fn metadata<P: Provider>(
 }
 
 #[derive(Deserialize)]
+pub struct MetadataRequest {
+    multipool: Address,
+}
+
+#[derive(Deserialize)]
 pub struct PositionsRequest {
     #[serde(rename = "a")]
     account: Address,
